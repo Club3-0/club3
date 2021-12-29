5d4ee88dc3a9266bc86de8ac986142c5c85bd25d
wb1110 <strengthcoachwb@gmail.com>
2021-12-28 18:03:13 -0600

    almost have parallax down

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/Landing.css';
 import child3 from '../img/child3.jpg';
 import child2 from '../img/child2.jpg';
-import lax from 'lax.js'
+import child1 from '../img/child1.jpg';
 
 
 const Landing = () => {
 
-    window.onload = function () {
-        lax.init()
-    
-        // Add a driver that we use to control our animations
-        lax.addDriver('scrollY', function () {
-          return window.scrollY
+    useEffect(() => {
+        const childPic = document.querySelector(".child")
+
+        const observer = new IntersectionObserver(
+            entries => {
+            entries.forEach(entry => {
+                entry.target.classList.toggle("child", entry.isIntersecting)
+                // const scrolled = window.pageYOffset;
+                // const rate = scrolled * -1;
+                // entry.target.style.transform = 'translate3d(0px, '+rate+', 0px)'
+            })
+        }, {
+            threshold: 1,
+            rootMargin: "-100px",
         })
-    
-        // Add animation bindings to elements
-        lax.addElements('.selector', {
-          scrollY: {
-            translateX: [
-              ["elInY", "elCenterY", "elOutY"],
-              [0, 'screenWidth/2', 'screenWidth'],
-            ]
-          }
-        })
-      }
+        console.log(childPic)
+        observer.observe(childPic)
+    }, [])
+
+
     return (
-        <div className="selector">
+        <div>
             <section className="main">
                 <section className="one">
                     <img src={child3}/>
@@ -46,16 +48,18 @@ const Landing = () => {
                     </div>
                 </section>
                 <section className="three">
-                    <div>
-                        <h1>About us</h1>
-                        <p>
-                        Frutti Felici Bakery is a European-styled café-patisserie where each customer is treated in a unique manner, respecting his/her individuality and needs. We offer various organic pastries, cookies, cakes, desserts, chocolate-dipped strawberries, and healthy smoothies. Our bakers use the finest quality ingredients and pride themselves on unique, time-tested recipes.  
-                        </p>
+                    <div className="contentContainer">
+                        <div>
+                            <h1>About us</h1>
+                            <p>
+                            Frutti Felici Bakery is a European-styled café-patisserie where each customer is treated in a unique manner, respecting his/her individuality and needs. We offer various organic pastries, cookies, cakes, desserts, chocolate-dipped strawberries, and healthy smoothies. Our bakers use the finest quality ingredients and pride themselves on unique, time-tested recipes.  
+                            </p>
+                        </div>
+                        <img className='child' src={child1}/>
                     </div>
-                    <img src="" alt="image" />
                 </section>
                 <section className="four">
-                    <div>
+                    <div className="lax_preset_fadeIn:234.25:0">
                         <h1>Order your custom cake today at Frutti Felici Bakery!</h1>
                         <p>Frutti Felici cakes are custom made cake art from your idea, theme, or picture. It takes time to manifest your idea into a work of art. Tap the links below to get a quote and order your custom made cake.</p>
                         <button>Get a quote</button>
