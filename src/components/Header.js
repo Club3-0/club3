6a98ce5b39d9d71e0dddc074fe746cd8458dfad1
wb1110 <strengthcoachwb@gmail.com>
2021-12-29 09:13:55 -0600

    nav bar updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,22 @@ import '../css/Header.css'
 const Header = () => {
     return (
         <div>
-            <div className="contactNav">Contact info that disappears on scroll</div>
+            <div className="contactNav">
+                <ul>
+                    <li className="contactLink"><i class="fas fa-phone"></i>555-555-5555</li>
+                    <li className="contactLink"><i class="far fa-envelope"></i>JENNIFERWEBSITE@GMAIL.COM</li>
+                    <li className="contactLink"><i class="far fa-clock"></i>MON-FRI: 9AM-5PM</li>
+                </ul>
+                <ul>
+                    <i className="fab fa-instagram"></i>
+                    <i className="fab fa-facebook-f"></i>
+                </ul>
+            </div>
             <div className="fixedNav">
-                <img src='' alt='Logo here' />
+                <div className='logoName'>
+                    <img src='' alt='Logo here' />
+                    <h1>Jen's Website</h1>
+                </div>
                 <nav>
                     <ul className="navLinks">
                         <li>Home</li>
