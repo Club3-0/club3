4c1bbf4c2836c2a3635ad665c66ed313130f29f2
wb1110 <strengthcoachwb@gmail.com>
2022-01-03 12:03:38 -0600

    fixed form error

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,31 +1,115 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import '../css/Register.css'
 
+const initialState = {
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    password: "",
+    confirm: "",
+    club: "",
+}
+
 const Register = () => {
+    const [state, setState] = useState(initialState);
+    const userInfo = {email: state.email, password: state.password}
+
+    const handleInputChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        setState({
+            ...state,
+            [name]: value
+        });
+    } 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        axios.post('https://reqres.in/api/register', {userInfo})
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
     return (
         <div className='formContainer'>
-            <form className='form'>
+            <form onSubmit={handleSubmit} className='form'>
                 <h2>Create your demo account</h2>
                 <p>Set up a time to discuss how we can best serve you</p>
                 <label className='nameLabel'> 
-                    <input className='nameInputField' type="text" placeholder='First name'/>
-                    <input className='nameInputField' type="text" placeholder='Last name'/>
+                    <input 
+                        autoComplete='off'
+                        className='nameInputField' 
+                        type="text" 
+                        placeholder='First name'
+                        name="firstName"
+                        value={state.firstName}
+                        onChange={handleInputChange} />
+                    <input 
+                        autoComplete='off'
+                        className='nameInputField' 
+                        type="text" 
+                        placeholder='Last name'
+                        name="lastName"
+                        value={state.lastName}
+                        onChange={handleInputChange} />
                 </label>
                 <label className='label'>
-                    <input className='inputField' type="text" placeholder='Phone number'/>
+                    <input 
+                        autoComplete='off'
+                        className='inputField' 
+                        type="text" 
+                        placeholder='Phone number'
+                        name="phone"
+                        value={state.phone}
+                        onChange={handleInputChange} />
                 </label>
                 <label className='label'>
-                    <input className='inputField' type="text" placeholder='Your email'/>
+                    <input 
+                        autoComplete='off'
+                        className='inputField' 
+                        type="text" 
+                        placeholder='Your email'
+                        name="email"
+                        value={state.email}
+                        onChange={handleInputChange} />
                     <p>This will be your username</p>
                 </label>
                 <label className='label'>
-                    <input className='inputField' type="password" placeholder='Password'/>
+                    <input 
+                        autoComplete='off'
+                        className='inputField' 
+                        type="password" 
+                        placeholder='Password'
+                        name="password"
+                        value={state.password}
+                        onChange={handleInputChange} />
                 </label>
                 <label className='label'>
-                    <input className='inputField' type="password" placeholder='Confirm Password'/>
+                    <input 
+                        autoComplete='off'
+                        className='inputField' 
+                        type="password" 
+                        placeholder='Confirm Password'
+                        name="confirm"
+                        value={state.confirm}
+                        onChange={handleInputChange}/>
                 </label>
                 <label className='label'>
-                    <input className='inputField' type="text" placeholder='Club name'/>
+                    <input 
+                        autoComplete='off'
+                        className='inputField' 
+                        type="text" 
+                        placeholder='Club name'
+                        name="club"
+                        value={state.club}
+                        onChange={handleInputChange} />
                 </label>
                 <button type="submit">Submit</button>
             </form>
