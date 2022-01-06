cbfe042484c1d928dd9b1f73d4acfc65bf5ae7c9
wb1110 <strengthcoachwb@gmail.com>
2022-01-06 05:16:42 -0600

    reverted alignment and kept new main text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,9 +18,9 @@ const Landing = () => {
 
         const tl = gsap.timeline()
             .fromTo('.club3', {y: '50%'}, {duration: 1, y: '0%'})
-            .fromTo('.solution', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
-            .fromTo('.problem', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
-            .fromTo('.demoButton', { opacity: '0%'}, {duration: 1, opacity: '100%'})
+            .fromTo('.line1', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
+            .fromTo('.line2', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
+            .fromTo('.line3', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
         
         const textTL1 = gsap.timeline({scrollTrigger: { trigger: ".two .textBox" } })
             .fromTo('.two .textBox', { opacity: '0%', y: '50%'}, {duration: 1, opacity: '100%', y: '0%'})
@@ -45,8 +45,9 @@ const Landing = () => {
                 <section className="one panel">
                     <div>
                         <h2 className='club3'>Club 3</h2>
-                        <h1 className='solution'>The solution,</h1>
-                        <h1 className='problem'>to the problem you actually have.</h1>
+                        <h1 className='line1'>Spend more time,</h1>
+                        <h1 className='line2'>doing what you love.</h1>
+                        <h3 className='line3'>Software built for directors, by a director.</h3>
                         <Link to="/register"><button className='demoButton'>Demo</button></Link>
                     </div>
                 </section> 
